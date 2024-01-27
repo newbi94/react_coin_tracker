adab06cd3b447d91782799694a791334d4fdac19
newbi94 <newbi94@gmail.com>
2024-01-27 22:22:37 +0900

    24.01.27
    coin traker challenge
    입력금액을 선택한 코인으로 전환시키는 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   const [money, setMoney] = useState();
-  const [selectCoin, setSelectCoin] = useState([]);
-  const [exchangeRate, setExchangeRate] = useState();
+  const [selectCoin, setSelectCoin] = useState({});
+  const [calculated,setCalculated] = useState();
+  //const [exchangeRate, setExchangeRate] = useState();
   
-  const onChangeMoney = (event) => {
-    const value = event.target.value;
+  const onChangeMoney = (event: ChangeEvent<HTMLInputElement>) => {
+    const value: string = event.target.value;
     const removedCommaValue: number = Number(value.replaceAll(",",""));
     setMoney(removedCommaValue.toLocaleString());
-  }//숫자 입력시 세자리마다 콤마를 찍어주는 코드를 추가했다.
-  //https://velog.io/@quack777/React-input-%EC%88%AB%EC%9E%90%EC%97%90-%EC%BD%A4%EB%A7%88-%EC%B0%8D%EC%96%B4%EC%84%9C-%EB%82%98%ED%83%80%EB%82%B4%EB%8A%94-%EB%B2%95
+  };
   
   const onChangeCoin = (event) =>{
     const selectedOption = JSON.parse(event.target.value)
-    setSelectCoin(selectedOption.quotes.USD.price)
-    console.log(selectedOption.quotes.USD.price)
-    }//객체화한 selectedOption자체를 setSelectCoin에 담아서
-    //selectCoin.quotes.USD.price를 사용하려 했지만 에러가 발생했고
-    //다시 JSON을 해야할수도 있을거 같길래 어차피 필요한건 금액이니 그것만
-    //빼다가 쓰기로 했다.
+    setSelectCoin(selectedOption)
+    }
+
+  const onChangeCountry = (event) =>{
+    console.log(event.target)
+  }
   
-  const calculateMoney = (money) =>{
-    console.log(money);
-  } //환율 계산기를 만들어야 한다.
+  const OnCalculateMoney = () =>{
+    console.log(money)
+    console.log(selectCoin)
+    const piece = money/selectCoin.quotes.USD.price
+    console.log(piece)
+    setCalculated(piece);
+}
 
   
   
@@ -34,7 +38,6 @@ function App() {
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
-        console.log(json)
         setLoading(false);
       });
   }, []);
@@ -48,8 +51,13 @@ function App() {
               placeholder="how much do you have?" 
               type="text"
               onChange={onChangeMoney}
-      /><button onClick ={() => calculateMoney(money)}/>
-              <div>{selectCoin*money}$</div>
+      /><button onClick ={OnCalculateMoney}>Trans</button>
+      
+      <select onChange={onChangeCountry}>
+        <option value="kormoney" key="kormoney">KOR₩</option>
+        <option value="usdmoney" key="usdmoney">USD$</option>
+      </select>
+      <div>        
       {loading ?  
       (<strong>Loading...</strong>): (
       <select onChange={onChangeCoin}>{coins.map((coin) => {
@@ -59,6 +67,10 @@ function App() {
       </select>
       )
       }
+      </div>
+      <div>How many of these coins can I have?
+                <div>{calculated}{selectCoin.symbol}</div>
+                </div>
     </div>
     
   );
